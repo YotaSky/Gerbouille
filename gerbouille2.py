924dca00e5e5c489bc83cd580f3b27083058d39b
YotaSky <30477784+YotaSky@users.noreply.github.com>
2017-08-14 21:53:32 +0200

    maj

--- gerbouille2.py
+++ gerbouille2.py
@@ -75,6 +75,7 @@ class Tools:
 		data = 'users'
 		file = open(os.path.join(os.path.sep,self.pathconf,data))
 		try:
+			print(csv.reader(file))
 			for user in csv.reader(file):
 				print(message.author.id)
 				while user[0] == message.author.id:
