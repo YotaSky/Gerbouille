48ad2511b32cc26c5c5aba123ec26413ac567681
YotaSky <30477784+YotaSky@users.noreply.github.com>
2017-08-14 20:05:48 +0200

    maj

--- gerbouille2.py
+++ gerbouille2.py
@@ -76,9 +76,9 @@ class Tools:
 		acl = open(os.path.join(os.path.sep,self.pathconf,data))
 		try:
 			reader = csv.reader(acl)
-			await client.send_message(message.channel,reader)
+			print(reader)
 			for user in reader:
-				await client.send_message(message.channel,user)
+				print(user)
 				if user[0] == message.author.id:
 					msg = 	"{name}" \
 							"{discord}" \
