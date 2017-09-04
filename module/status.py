2be2688945cbdde73f6ed271cc83119e4246af8f
YotaSky <30477784+YotaSky@users.noreply.github.com>
2017-09-04 20:23:46 +0200

    fix

--- module/status.py
+++ module/status.py
@@ -91,6 +91,6 @@ class Status: # Définition des méthodes de fonction de Gerbouille
                 lst = ' ('+', '.join(listplayers)+')'
             else: 
                 lst = ''
-            info.append('**{}** ({} ms): {} survivant(s) en ligne {}\n'.format(config['SessionName'], round(float(ping)), str(len(listplayers)), lst))
+            info.append('**{}** ({} ms - {}): {} survivant(s) en ligne {}\n'.format(config['SessionName'], round(float(ping)), version, str(len(listplayers)), lst))
             #info.append('**{} - ({})**: {} survivant(s) en ligne {}\n'.format(config['SessionName'], version, str(len(listplayers)), lst))
         return ''.join(info)
