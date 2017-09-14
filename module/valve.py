bd883c79d0154636c476f5fb2470ba9871cb5109
YotaSky <30477784+YotaSky@users.noreply.github.com>
2017-09-14 15:46:03 +0200

    str

--- module/valve.py
+++ module/valve.py
@@ -6,8 +6,7 @@
 import valve.source.a2s
 
 def request(message, ip, port):
-    server = valve.source.a2s.ServerQuerier(address=(ip, port), timeout = 5.0)
-    print (server)
+    server = valve.source.a2s.ServerQuerier(address=(str(ip), port), timeout = 5.0)
     name = server.info()["server_name"].format(message)    
     return name
 
