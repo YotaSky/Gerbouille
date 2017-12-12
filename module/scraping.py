f4b9a5823fab3b167309a66558d855da51e4d31d
YotaSky <30477784+YotaSky@users.noreply.github.com>
2017-12-12 17:29:47 +0100

    update scraping

--- module/scraping.py
+++ module/scraping.py
@@ -6,4 +6,4 @@ from bs4 import BeautifulSoup
 def insultron():
     page = requests.get("http://insultron.fr/")
     soup = BeautifulSoup(page.content, 'html.parser')
-    return soup.find_all('p')[0].get_text()
+    return soup.find_all('p')[1].get_text()
