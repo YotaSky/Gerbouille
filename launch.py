1672abe156e669fb6ba1d697cce4c60027c2133e
YotaSky <30477784+YotaSky@users.noreply.github.com>
2018-07-08 18:14:48 +0200

    admin menu

--- launch.py
+++ launch.py
@@ -56,10 +56,30 @@ async def on_message(message):
 		servers = Status().instances(message, True)
 		await client.send_message(message.channel,servers)
 		await client.send_message(message.channel,"Quelle instance veux-tu administrer ?")
+		input_srv = await client.wait_for_message(timeout=120.0, author=message.author)
+        
+        admin_msg = "{start}" \
+            		"{stop}" \
+            		"{restart}" \
+            		"{saveworld}" \
+            		"{wipedinos}" \
+            		"{installmod}" \
+            		"{removemod}" \
+            		"{backup}" \
+    		.format(start="**!start** - Démarrer instance\n",
+            		stop="**!stop** - Arrêter instance\n",
+            		restart="**!restart** - Redémarrer instance [OFF]\n",
+            		saveworld="**!saveworld** - Sauvegarde Map [OFF]\n",
+            		wipedinos="**!wipedinos** - Détruire tous les dinos sauvages [OFF]\n",
+            		installmod="**!installmod** - Installer un nouveau Mod [OFF]\n",
+            		removemod="**!removemod** - Supprimer un Mod [OFF]\n",
+            		backup="**!backup** - Sauvegarde données Map [OFF]\n",
+            		)
+    	await client.send_message(message.channel,admin_msg)
+    	input_opt = await client.wait_for_message(timeout=120.0, author=message.author)
 
-		msg = await client.wait_for_message(timeout=120.0, author=message.author)
-		launch = CmdServer().admin(msg.content)
-		await client.send_message(message.channel,launch)
+		#launch = CmdServer().admin(msg.content)
+		#await client.send_message(message.channel,launch)
 
 	
 	if message.content.startswith('tagle'):
