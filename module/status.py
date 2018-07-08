017d643c3ffe5e1d0418d4f34e68ff8729ae220d
YotaSky <30477784+YotaSky@users.noreply.github.com>
2018-07-08 15:27:03 +0200

    up

--- module/status.py
+++ module/status.py
@@ -95,5 +95,4 @@ class Status: # Définition des méthodes de fonction de Gerbouille
                 info.append('{}. **{}** {} survivant(s) en ligne\n```'.format(num,request, str(len(listplayers))))
             else:
                 info.append('**{}** ({}): {} survivant(s) en ligne {}\n'.format(request, connect, str(len(listplayers)), lst))
-            txt = '```markdown\n#Liste des instances ARK\n{}\n```'.format(''.join(info))
-        return txt
+        return ''.join(info)
